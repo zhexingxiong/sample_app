8a2e9dacfafaae92b6f335a36353b717c054f2d5
Xing Xiong <xiongxing@gmail.com>
2012-01-13 15:48:45 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  
+  match '/signup', :to => 'users#new'
+  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   root :to => 'pages#home'
+
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -59,4 +69,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
+
